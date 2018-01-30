1bc8387ea948c883b80e12f27fa1c58403b2a261
varunmishra028 <varunmishra028@gmail.com>
2018-01-30 19:54:28 +0530

    add main

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from main_app import views
 
 urlpatterns = [
+    url(r'/',views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^index/',views.index),
 ]
